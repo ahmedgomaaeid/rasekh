ebabd249ca0dce8bc34a49915cac13c74679fc33
ahmedgomaaeid <pahmedgomaaeid@gmail,com>
2023-06-25 21:19:42 +0300

    common question

--- app/Http/Controllers/Student/IndexController.php
+++ app/Http/Controllers/Student/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\Catogry\MainCategory;
+use App\Models\CommonQuestion;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -12,6 +13,7 @@ class IndexController extends Controller
     {
         
         $mainCategories = MainCategory::where('status', 1)->get();
+        $questions = CommonQuestion::all();
         return view('student.index', compact('mainCategories'));
     }
 }
