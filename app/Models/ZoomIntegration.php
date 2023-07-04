f1a177c30085f059f3ea5d0dd4ad28e3193fbbac
ahmedgomaaeid <pahmedgomaaeid@gmail,com>
2023-07-04 21:37:28 +0300

    zoom meeting

--- app/Models/ZoomIntegration.php
+++ app/Models/ZoomIntegration.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ZoomIntegration extends Model
 {
     use HasFactory;
-    protected $fillable = ['teacher_id', 'oauth_client_id', 'sdk_client_id', 'sdk_client_secret' ,'oauth_client_secret'];
+    protected $fillable = ['teacher_id', 'oauth_client_id', 'sdk_client_id', 'sdk_client_secret' , 'oauth_client_secret'];
     public function teacher()
     {
         return $this->belongsTo(Teacher::class);
