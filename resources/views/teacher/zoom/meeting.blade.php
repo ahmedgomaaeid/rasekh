1adb68b9918c70e9cd004850f5fa322e4a82fcfa
ahmedgomaaeid <pahmedgomaaeid@gmail,com>
2023-07-16 05:22:43 +0300

    zoom meeting

--- resources/views/teacher/zoom/meeting.blade.php
+++ resources/views/teacher/zoom/meeting.blade.php
@@ -11,11 +11,7 @@
 </head>
 
 <body id="body">
-    <button id="btn-start-recording" style="z-index:1000; position:fixed; color:white;">Start Recording</button>
-    <button id="btn-stop-recording" style="z-index:1000; position:fixed; color:white; left:300px;" disabled>Stop Recording</button>
-    <canvas id="background-canvas" style="position:absolute; top:-99999999px; left:-9999999999px;"></canvas>
-    <script src="{{route('index')}}/zoom/RecordRTC.js"></script>
-    <script src="https://www.webrtc-experiment.com/html2canvas.min.js"></script>
+
     <script>
         var role = 1;
         window.addEventListener('DOMContentLoaded', function(event) {
@@ -33,70 +29,7 @@
     <script src="{{route('index')}}/zoom/js/tool.js"></script>
     <script src="{{route('index')}}/zoom/js/vconsole.min.js"></script>
     <script src="{{route('index')}}/zoom/js/meeting.js"></script>
-    <script>
-        var elementToRecord = document.getElementById('body');
-        var canvas2d = document.getElementById('background-canvas');
-        var context = canvas2d.getContext('2d');
 
-        canvas2d.width = elementToRecord.clientWidth;
-        canvas2d.height = elementToRecord.clientHeight;
-
-        var isRecordingStarted = false;
-        var isStoppedRecording = false;
-
-        setInterval(function looper() {
-            if (!isRecordingStarted) {
-                return;
-            }
-
-            html2canvas(elementToRecord).then(function(canvas) {
-                context.clearRect(0, 0, canvas2d.width, canvas2d.height);
-                context.drawImage(canvas, 0, 0, canvas2d.width, canvas2d.height);
-
-                if (isStoppedRecording) {
-                    return;
-                }
-            });
-        }, 16.67); // 1000ms / 60fps = 16.67ms
-
-        var recorder = new RecordRTC(canvas2d, {
-            type: 'canvas'
-            , video: {
-                width: 1280
-                , height: 720
-            }
-        });
-
-        document.getElementById('btn-start-recording').onclick = function() {
-            this.disabled = true;
-
-            isStoppedRecording = false;
-            isRecordingStarted = true;
-
-            recorder.startRecording();
-            document.getElementById('btn-stop-recording').disabled = false;
-        };
-
-        document.getElementById('btn-stop-recording').onclick = function() {
-            this.disabled = true;
-
-            recorder.stopRecording(function() {
-                isRecordingStarted = false;
-                isStoppedRecording = true;
-
-                var blob = recorder.getBlob();
-                var url = URL.createObjectURL(blob);
-                var a = document.createElement('a');
-                document.body.appendChild(a);
-                a.style = 'display: none';
-                a.href = url;
-                a.download = 'recorded-video.webm';
-                a.click();
-                window.URL.revokeObjectURL(url);
-            });
-        };
-
-    </script>
 </body>
 
 </html>
