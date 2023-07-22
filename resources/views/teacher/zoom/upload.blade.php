71324c3882f6af93b28355af3e2028199f56c6c8
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-07-22 06:02:55 +0300

    zoom meeting

--- resources/views/teacher/zoom/upload.blade.php
+++ resources/views/teacher/zoom/upload.blade.php
@@ -1,4 +1,5 @@
 <form action="{{route('post.teacher.zoom-meeting.upload')}}" method="POST">
+    @csrf
     <input type="file" id="fileInput" name="fileInput" webkitdirectory>
     <input type="submit" value="Upload">
 </form>
