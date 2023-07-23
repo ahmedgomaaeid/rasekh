b7591705c7bdb1c28373c24c0f6db31a0fc2a0fb
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-07-23 14:13:52 +0300

    zoom meeting

--- resources/views/teacher/zoom/upload.blade.php
+++ resources/views/teacher/zoom/upload.blade.php
@@ -1,12 +1,12 @@
 <form action="{{route('post.teacher.zoom-meeting.upload')}}" method="POST" enctype="multipart/form-data">
     @csrf
-    <p id="folder_name">
-    <input type="file" id="folder" name="folder[]" webkitdirectory directory multiple onchange="folder()">
+    <p id="folder_name"></p>
+    <input type="file" id="folder" name="folder[]" webkitdirectory directory multiple onchange="folderr()">
     <input type="button" value="Upload">
 </form>
 <script>
     //show folde name when select folder
-    function folder() {
+    function folderr() {
         var x = document.getElementById("folder").files;
         var txt = "";
         var i;
