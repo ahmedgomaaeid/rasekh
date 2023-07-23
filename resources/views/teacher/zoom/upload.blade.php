ea35f06716f93d1f1b8c6ce0606b2fc44977edf9
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-07-23 17:49:36 +0300

    zoom meeting

--- resources/views/teacher/zoom/upload.blade.php
+++ resources/views/teacher/zoom/upload.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <input type="hidden" name="folder_name" id="folder_name">
     <input type="file" id="folder" name="folder[]" webkitdirectory directory multiple onchange="folderr()">
-    <input type="button" value="Upload">
+    <input type="submit" value="Upload">
 </form>
 <script>
     //show folde name when select folder
