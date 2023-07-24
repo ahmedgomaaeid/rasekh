5584784f2a83317a887744cc026a6ca0cbd75611
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-07-24 19:56:23 +0300

    zoom meeting

--- resources/views/teacher/zoom/upload.blade.php
+++ resources/views/teacher/zoom/upload.blade.php
@@ -1,55 +1,7 @@
  @extends('layouts.teacherdashboard')
 
  @section('content')
- <style>
-     .drop-zone {
-         height: 200px;
-         padding: 25px;
-         display: flex;
-         align-items: center;
-         justify-content: center;
-         text-align: center;
-         font-family: "Quicksand", sans-serif;
-         font-weight: 500;
-         font-size: 20px;
-         cursor: pointer;
-         color: #cccccc;
-         border: 4px dashed var(--primary-bg-color);
-         border-radius: 10px;
-     }
 
-     .drop-zone--over {
-         border-style: solid;
-     }
-
-     .drop-zone__input {
-         display: none;
-     }
-
-     .drop-zone__thumb {
-         width: 100%;
-         height: 100%;
-         border-radius: 10px;
-         overflow: hidden;
-         background-color: #cccccc;
-         background-size: cover;
-         position: relative;
-     }
-
-     .drop-zone__thumb::after {
-         content: attr(data-label);
-         position: absolute;
-         bottom: 0;
-         left: 0;
-         width: 100%;
-         padding: 5px 0;
-         color: #ffffff;
-         background: rgba(0, 0, 0, 0.75);
-         font-size: 14px;
-         text-align: center;
-     }
-
- </style>
  <div class="main-content app-content mt-0">
      <div class="side-app">
 
@@ -79,11 +31,7 @@
                              <div class="card-body">
                                  <input type="hidden" name="folder_name" id="folder_name">
 
-                                 <div class="drop-zone">
-                                     <span class="drop-zone__prompt">اسحب مجلد التسجيل هنا او اضغط لتحديد المجلد</span>
-                                     <div><i class="fa fa-folder-open-o" aria-hidden="true" style="display:block; font-size: 47px;"></i></div>
-                                     <input type="file" class="drop-zone__input" id="folder" name="folder" webkitdirectory directory multiple onchange="folderr()">
-                                 </div>
+                                 <input type="file" class="dropify" data-bs-height="180" id="folder" name="folder" webkitdirectory directory multiple onchange="folderr()">
 
                                  @error('lesson_name')
                                  <div class="invalid-feedback" style="display:block;">{{$message}}</div>
@@ -112,83 +60,7 @@
      </div>
  </div>
  <script>
-     document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-         const dropZoneElement = inputElement.closest(".drop-zone");
 
-         dropZoneElement.addEventListener("click", (e) => {
-             inputElement.click();
-         });
-
-         inputElement.addEventListener("change", (e) => {
-             if (inputElement.files.length) {
-                 updateThumbnail(dropZoneElement, inputElement.files[0]);
-             }
-             folderr();
-         });
-
-         dropZoneElement.addEventListener("dragover", (e) => {
-             e.preventDefault();
-             dropZoneElement.classList.add("drop-zone--over");
-             folderr();
-         });
-
-         ["dragleave", "dragend"].forEach((type) => {
-             dropZoneElement.addEventListener(type, (e) => {
-                 dropZoneElement.classList.remove("drop-zone--over");
-                 folderr();
-             });
-         });
-
-         dropZoneElement.addEventListener("drop", (e) => {
-             e.preventDefault();
-
-             if (e.dataTransfer.files.length) {
-                 inputElement.files = e.dataTransfer.files;
-                 updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-             }
-
-             dropZoneElement.classList.remove("drop-zone--over");
-             folderr();
-         });
-     });
-
-     /**
-      * Updates the thumbnail on a drop zone element.
-      *
-      * @param {HTMLElement} dropZoneElement
-      * @param {File} file
-      */
-     function updateThumbnail(dropZoneElement, file) {
-        
-         let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-         // First time - remove the prompt
-         if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-             dropZoneElement.querySelector(".drop-zone__prompt").remove();
-         }
-
-         // First time - there is no thumbnail element, so lets create it
-         if (!thumbnailElement) {
-             thumbnailElement = document.createElement("div");
-             thumbnailElement.classList.add("drop-zone__thumb");
-             dropZoneElement.appendChild(thumbnailElement);
-         }
-
-         thumbnailElement.dataset.label = file.name;
-
-         // Show thumbnail for image files
-         if (file.type.startsWith("image/")) {
-             const reader = new FileReader();
-
-             reader.readAsDataURL(file);
-             reader.onload = () => {
-                 thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-             };
-         } else {
-             thumbnailElement.style.backgroundImage = null;
-         }
-         folderr();
-     }
 
 
      function folderr() {
