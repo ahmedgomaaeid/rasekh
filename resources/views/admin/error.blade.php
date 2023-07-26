79f748dfc541e3b2f49bf9e55227a1b9ed9aa614
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-07-26 06:23:36 +0300

    zoom meeting

--- resources/views/admin/error.blade.php
+++ resources/views/admin/error.blade.php
@@ -26,7 +26,7 @@
                          <div class="card text-danger bg-danger-transparent card-transparent">
                              <div class="card-body">
                                  <h4 class="card-title">{{$error->created_at}}</h4>
-                                 <p class="card-text">{!!$error->message!!}</p>
+                                 <p class="card-text">{!!$error->text!!}</p>
                              </div>
                          </div>
                      </div>
