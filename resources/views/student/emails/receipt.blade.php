d0039787360c58f653c9e720f5d9f4d79779dd1c
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-07-28 23:16:49 +0300

    recapatcha

--- resources/views/student/emails/receipt.blade.php
+++ resources/views/student/emails/receipt.blade.php
@@ -1,197 +1,325 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receipt Sample</title>
-    <link rel="stylesheet" href="style.css">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
 
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Source Sans Pro', sans-serif;
-}
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Email Receipt</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style type="text/css">
+  /**
+   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
+   */
+  @media screen {
+    @font-face {
+      font-family: 'Source Sans Pro';
+      font-style: normal;
+      font-weight: 400;
+      src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+    }
 
-.container {
-    display: block;
-    width: 100%;
-    background: #fff;
-    max-width: 350px;
-    padding: 25px;
-    margin: 50px auto 0;
-    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
-}
+    @font-face {
+      font-family: 'Source Sans Pro';
+      font-style: normal;
+      font-weight: 700;
+      src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+    }
+  }
 
-.receipt_header {
-    padding-bottom: 40px;
-    border-bottom: 1px dashed #000;
-    text-align: center;
-}
+  /**
+   * Avoid browser level font resizing.
+   * 1. Windows Mobile
+   * 2. iOS / OSX
+   */
+  body,
+  table,
+  td,
+  a {
+    -ms-text-size-adjust: 100%; /* 1 */
+    -webkit-text-size-adjust: 100%; /* 2 */
+  }
 
-.receipt_header h1 {
-    font-size: 20px;
-    margin-bottom: 5px;
-    text-transform: uppercase;
-}
+  /**
+   * Remove extra space added to tables and cells in Outlook.
+   */
+  table,
+  td {
+    mso-table-rspace: 0pt;
+    mso-table-lspace: 0pt;
+  }
 
-.receipt_header h1 span {
-    display: block;
-    font-size: 25px;
-}
+  /**
+   * Better fluid images in Internet Explorer.
+   */
+  img {
+    -ms-interpolation-mode: bicubic;
+  }
 
-.receipt_header h2 {
-    font-size: 14px;
-    color: #727070;
-    font-weight: 300;
-}
+  /**
+   * Remove blue links for iOS devices.
+   */
+  a[x-apple-data-detectors] {
+    font-family: inherit !important;
+    font-size: inherit !important;
+    font-weight: inherit !important;
+    line-height: inherit !important;
+    color: inherit !important;
+    text-decoration: none !important;
+  }
 
-.receipt_header h2 span {
-    display: block;
-}
+  /**
+   * Fix centering issues in Android 4.4.
+   */
+  div[style*="margin: 16px 0;"] {
+    margin: 0 !important;
+  }
 
-.receipt_body {
-    margin-top: 25px;
-}
+  body {
+    width: 100% !important;
+    height: 100% !important;
+    padding: 0 !important;
+    margin: 0 !important;
+  }
 
-table {
-    width: 100%;
-}
+  /**
+   * Collapse table borders to avoid space between cells.
+   */
+  table {
+    border-collapse: collapse !important;
+  }
 
-thead, tfoot {
-    position: relative;
-}
+  a {
+    color: #1a82e2;
+  }
 
-thead th:not(:last-child) {
-    text-align: left;
-}
+  img {
+    height: auto;
+    line-height: 100%;
+    text-decoration: none;
+    border: 0;
+    outline: none;
+  }
+  </style>
 
-thead th:last-child {
-    text-align: right;
-}
-
-thead::after {
-    content: '';
-    width: 100%;
-    border-bottom: 1px dashed #000;
-    display: block;
-    position: absolute;
-}
-
-tbody td:not(:last-child), tfoot td:not(:last-child) {
-    text-align: left;
-}
-
-tbody td:last-child, tfoot td:last-child{
-    text-align: right;
-}
-
-tbody tr:first-child td {
-    padding-top: 15px;
-}
-
-tbody tr:last-child td {
-    padding-bottom: 15px;
-}
-
-tfoot tr:first-child td {
-    padding-top: 15px;
-}
-
-tfoot::before {
-    content: '';
-    width: 100%;
-    border-top: 1px dashed #000;
-    display: block;
-    position: absolute;
-}
-
-tfoot tr:first-child td:first-child, tfoot tr:first-child td:last-child {
-    font-weight: bold;
-    font-size: 20px;
-}
-
-.date_time_con {
-    display: flex;
-    justify-content: center;
-    column-gap: 25px;
-}
-
-.items {
-    margin-top: 25px;
-}
-
-h3 {
-    border-top: 1px dashed #000;
-    padding-top: 10px;
-    margin-top: 25px;
-    text-align: center;
-    text-transform: uppercase;
-}
-    </style>
 </head>
-<body>
-<div class="container">
-    
-    <div class="receipt_header">
-    <h1>ايصال شراء <span>منصة راسخ التعليمية</span></h1>
-    <a href="{{route('index')}}">{{route('index')}}</a>
-    </div>
-    
-    <div class="receipt_body">
+<body style="background-color: #D2C7BA;">
 
-        <div class="date_time_con">
-            <div class="date">{{ $order->created_at->format('d/m/Y') }}</div>
-            <div class="time">{{ $order->created_at->format('h:i A') }}</div>
-        </div>
+  <!-- start preheader -->
+  <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+    A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
+  </div>
+  <!-- end preheader -->
 
-        <div class="items">
-            <table>
-        
-                <thead>
-                    <th>رقم الطلب</th>
-                    <th>العنصر</th>
-                    <th>السعر</th>
-                </thead>
-        
-                <tbody>
-                    <tr>
-                        <td>{{$order->id}}</td>
-                        <td>{{$order->course->name}} للاستاذ {{$order->course->steacher->name}}</td>
-                        <td>{{$order->course_price}} شيكل</td>
-                    </tr>
-                </tbody>
+  <!-- start body -->
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
 
-                <tfoot>
-                    <tr>
-                        <td>اسم الطالب</td>
-                        <td></td>
-                        <td>{{Auth::guard('student')->user()->name}}</td>
-                    </tr>
-                    <tr>
-                        <td>البريد الالكترونى</td>
-                        <td></td>
-                        <td>{{Auth::guard('student')->user()->email}}</td>
-                    </tr>
-                    <tr>
-                        <td>رقم الهاتف</td>
-                        <td></td>
-                        <td>{{Auth::guard('student')->user()->phone}}</td>
-                    </tr>
-                </tfoot>
+    <!-- start logo -->
+    <tr>
+      <td align="center" bgcolor="#D2C7BA">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="center" valign="top" style="padding: 36px 24px;">
+              <a href="https://sendgrid.com" target="_blank" style="display: inline-block;">
+                <img src="./img/paste-logo-light@2x.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+              </a>
+            </td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end logo -->
 
-            </table>
-        </div>
+    <!-- start hero -->
+    <tr>
+      <td align="center" bgcolor="#D2C7BA">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Thank you for your order!</h1>
+            </td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end hero -->
 
-    </div>
+    <!-- start copy block -->
+    <tr>
+      <td align="center" bgcolor="#D2C7BA">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
 
+          <!-- start copy -->
+          <tr>
+            <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              <p style="margin: 0;">Here is a summary of your recent order. If you have any questions or concerns about your order, please <a href="https://sendgrid.com">contact us</a>.</p>
+            </td>
+          </tr>
+          <!-- end copy -->
 
-    <h3>شكرا لك</h3>
+          <!-- start receipt table -->
+          <tr>
+            <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td align="left" bgcolor="#D2C7BA" width="75%" style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"><strong>Order #</strong></td>
+                  <td align="left" bgcolor="#D2C7BA" width="25%" style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"><strong>0000224</strong></td>
+                </tr>
+                <tr>
+                  <td align="left" width="75%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">Item</td>
+                  <td align="left" width="25%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">$24.00</td>
+                </tr>
+                <tr>
+                  <td align="left" width="75%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">Item</td>
+                  <td align="left" width="25%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">$24.00</td>
+                </tr>
+                <tr>
+                  <td align="left" width="75%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">Shipping</td>
+                  <td align="left" width="25%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">$6.00</td>
+                </tr>
+                <tr>
+                  <td align="left" width="75%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">Sales Tax</td>
+                  <td align="left" width="25%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">$0.00</td>
+                </tr>
+                <tr>
+                  <td align="left" width="75%" style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>Total</strong></td>
+                  <td align="left" width="25%" style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>$54.00</strong></td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end reeipt table -->
 
-</div>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end copy block -->
+
+    <!-- start receipt address block -->
+    <tr>
+      <td align="center" bgcolor="#D2C7BA" valign="top" width="100%">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="center" valign="top" style="font-size: 0; border-bottom: 3px solid #d4dadf">
+              <!--[if (gte mso 9)|(IE)]>
+              <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+              <tr>
+              <td align="left" valign="top" width="300">
+              <![endif]-->
+              <div style="display: inline-block; width: 100%; max-width: 50%; min-width: 240px; vertical-align: top;">
+                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 300px;">
+                  <tr>
+                    <td align="left" valign="top" style="padding-bottom: 36px; padding-left: 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                      <p><strong>Delivery Address</strong></p>
+                      <p>1234 S. Broadway Ave<br>Unit 2<br>Denver, CO 80211</p>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <!--[if (gte mso 9)|(IE)]>
+              </td>
+              <td align="left" valign="top" width="300">
+              <![endif]-->
+              <div style="display: inline-block; width: 100%; max-width: 50%; min-width: 240px; vertical-align: top;">
+                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 300px;">
+                  <tr>
+                    <td align="left" valign="top" style="padding-bottom: 36px; padding-left: 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+                      <p><strong>Billing Address</strong></p>
+                      <p>1234 S. Broadway Ave<br>Unit 2<br>Denver, CO 80211</p>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <!--[if (gte mso 9)|(IE)]>
+              </td>
+              </tr>
+              </table>
+              <![endif]-->
+            </td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end receipt address block -->
+
+    <!-- start footer -->
+    <tr>
+      <td align="center" bgcolor="#D2C7BA" style="padding: 24px;">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+
+          <!-- start permission -->
+          <tr>
+            <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+              <p style="margin: 0;">You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>
+            </td>
+          </tr>
+          <!-- end permission -->
+
+          <!-- start unsubscribe -->
+          <tr>
+            <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+              <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
+              <p style="margin: 0;">Paste 1234 S. Broadway St. City, State 12345</p>
+            </td>
+          </tr>
+          <!-- end unsubscribe -->
+
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end footer -->
+
+  </table>
+  <!-- end body -->
 
 </body>
 </html>
